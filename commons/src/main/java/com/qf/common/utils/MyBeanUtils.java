eabc5ad9f9cdf1d01afde3e1468f737e6e2f8acc
pengguoyi <3158944308@qq.com>
2023-09-29 08:46:46 +0800

    对工具类进行修改

--- commons/src/main/java/com/qf/common/utils/MyBeanUtils.java
+++ commons/src/main/java/com/qf/common/utils/MyBeanUtils.java
@@ -2,6 +2,7 @@ package com.qf.common.utils;
 
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.BeanUtils;
 
 import java.util.List;
@@ -26,6 +27,19 @@ public class MyBeanUtils extends BeanUtils {
         T t = target.get();
         copyProperties(source, t);
         return t;
+    }
+
+    /**
+     *将Page<po>  -->  转换为  Page<Vo>
+     */
+    public static <S,T> Page<T> copyList(Page<S> source, Page<T> page,Supplier<T> target){
+        //第一步复制除了集合的其它属性
+        copyProperties(source,page);
+        //第二步复制集合
+        List<T> list = beanToList(source.getRecords(), target);
+        //第三步给目标vo对象赋值
+        page.setRecords(list);
+        return page;
     }
 
 }
